98f7fa5f69f39143cb08549b6b037dda3e7350d0
Terry Howe <terrylhowe@gmail.com>
2017-05-18 10:23:21 -0600

    checkpoint on keypress events

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+function TextView(props) {
+  if (!props.text) {
+    return null;
+  }
+
+  return (
+    <pre>
+      {props.text}
+    </pre>
+  );
+}
+
+
+class ChildComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {x: 0, y: 0, direction: 'N', text: 'AAAAA'};
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Looking {this.state.direction} from {this.state.x},{this.state.y}.</h1>
+        <TextView text={this.state.text} />
+        <b>w:</b> forward<br/>
+        <b>a:</b> left<br/>
+        <b>d:</b> right<br/>
+        <b>s:</b> backward<br/>
       </div>
     );
   }
 }
 
-export default App;
+class Maze extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {x: 0, y: 0, direction: 'N', text: 'AAAAA'};
+  }
+
+  handleClick = (e) => {
+    this.setState({x: 4});
+  }
+
+  handleKeyDown = (e) => {
+    this.setState({direction: 'S'});
+    this.setState({x: 2});
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    return (
+      <ChildComponent handleKeyDown={() => this.handleKeyDown()} />
+    );
+  }
+}
+
+export default Maze;
