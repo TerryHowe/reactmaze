bbc12d4f1a25c2058ad95a7ae4938aeda6ba09c1
Terry Howe <terrylhowe@gmail.com>
2017-05-22 12:28:22 -0600

    get right indention

--- src/TextView.js
+++ src/TextView.js
@@ -242,64 +242,64 @@ function TextView(props) {
 
   let room = props.rooms[props.x][props.y];
   let direction = props.direction;
-    let t = [VIEW.split("")]
-    let left_room = room.goLeft(direction)
+  let t = [VIEW.split("")]
+  let left_room = room.goLeft(direction)
+  forward_room = room.go_forward(direction)
 /*
-    forward_room = room.go_forward(direction)
-    right_room = room.go_right(direction)
+  right_room = room.go_right(direction)
 */
-    if (typeof left_room === 'undefined') {
-      t.push(LEFT.split(""))
+  if (typeof left_room === 'undefined') {
+    t.push(LEFT.split(""))
+  }
+/*
+  } else {
+    if (typeof left_room.go_forward(direction) === 'undefined') {
+      t.append(LEFT_FORWARD_RIGHT)
+    }
+  }
+*/
+  if (typeof forward_room === 'undefined') {
+    t.append(FORWARD)
+  } else {
+/*
+    forward_left_room = forward_room.go_left(direction)
+    if (typeof forward_left_room === 'undefined') {
+      t.append(FORWARD_LEFT)
+    } else {
+      if (typeof forward_left_room.go_forward(direction) === 'undefined') {
+        t.append(FORWARD_LEFT_FORWARD_RIGHT)
+      }
+    }
+    if (typeof forward_room.go_forward(direction) === 'undefined') {
+      t.append(FORWARD_FORWARD)
+    }
+    forward_right_room = forward_room.go_right(direction)
+    if (typeof forward_right_room === 'undefined') {
+      t.append(FORWARD_RIGHT)
+    } else {
+      if (typeof forward_right_room.go_forward(direction) === 'undefined') {
+        t.append(FORWARD_RIGHT_FORWARD_LEFT)
+      }
     }
 /*
-    else
-      if left_room.go_forward(direction).nil?
-        t.append(LEFT_FORWARD_RIGHT)
-      end
-    end
-    if forward_room.nil?
-      t.append(FORWARD)
-    else
-      forward_left_room = forward_room.go_left(direction)
-      if forward_left_room.nil?
-        t.append(FORWARD_LEFT)
-      else
-        if forward_left_room.go_forward(direction).nil?
-          t.append(FORWARD_LEFT_FORWARD_RIGHT)
-        end
-      end
-      if forward_room.go_forward(direction).nil?
-        t.append(FORWARD_FORWARD)
-      end
-      forward_right_room = forward_room.go_right(direction)
-      if forward_right_room.nil?
-        t.append(FORWARD_RIGHT)
-      else
-        if forward_right_room.go_forward(direction).nil?
-          t.append(FORWARD_RIGHT_FORWARD_LEFT)
-        end
-      end
-    end
-    if right_room.nil?
-      t.append(RIGHT)
-    else
-      if right_room.go_forward(direction).nil?
-        t.append(RIGHT_FORWARD_LEFT)
-      end
-    end
-    r3 = (t.map{|x| x.split("")}).transpose
-    return (r3.map{|x| x.max}).join("")
-  end
-end
+  }
+/*
+  if (typeof right_room === 'undefined') {
+    t.append(RIGHT)
+  } else {
+    if (typeof right_room.go_forward(direction) === 'undefined') {
+      t.append(RIGHT_FORWARD_LEFT)
+    }
+  }
 */
   let zip= rows=>rows[0].map((_,c)=>rows.map(row=>row[c]))
   let z = zip(t);
   let result = z.map(row => {return row.reduce((a,b) => {return a>b?a:b;})});
 
   return (
-    <pre>
-      {result}
-    </pre>
+<pre>
+{result}
+</pre>
   );
 }
 
