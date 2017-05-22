b6d8369f8de7015830f24786f9238f2ad896c6ea
Terry Howe <terrylhowe@gmail.com>
2017-05-22 08:58:07 -0600

    change passages to hash of rooms

--- src/Room.js
+++ src/Room.js
@@ -3,11 +3,49 @@ class Room {
     this.id = room.id;
     this.x = room.x;
     this.y = room.y;
-    this.passages = [];
+    this.passages = {};
   }
 
   toString = () => {
-    return(this.x.toString() + "," + this.y.toString() + ": " + this.passages.join(';'));
+    var passageString = Object.keys(this.passages);
+    return(this.x.toString() + "," + this.y.toString() + ": " + passageString);
+  }
+
+  addDestination = (passages) => {
+    passages.forEach(passage => {
+      if (this.id === passage.destination_id) {
+        passage.destination = this;
+      }
+    })
+  }
+
+  addPassages = (passages) => {
+    passages.forEach(passage => {
+      if (this.id === passage.source_id) {
+        this.passages[passage.direction] = passage.destination;
+      }
+    })
+  }
+
+  getBackwardDirection = (direction) => {
+    switch (direction) {
+    case 'N':
+      return('S');
+    case 'E':
+      return('W');
+    case 'S':
+      return('N');
+    default:
+      return('E');
+    }
+  }
+
+  goForward = (direction) => {
+    return(this.passages[direction]);
+  }
+
+  goBackward = (direction) => {
+    return(this.passages[this.getBackwardDirection(direction)]);
   }
 }
 
